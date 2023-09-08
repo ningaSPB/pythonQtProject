921c90a5e27adf99036c665b4c781b7e4b12f1b8
timur <titovtimur2008@gmail.com>
2023-09-08 12:28:36 +0300

    edited

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 from PyQt5.QtCore import Qt
 from random import randint
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
-def show_winer():
-    winner = randint(1, 100)
-    label1.setText(str(winner))
 
-app = QApplication([])
-mw = QWidget()
-mw.show()
-mw.setWindowTitle('кот')
-label = QLabel('нажми чтобы узнать победителя')
-vline = QVBoxLayout()
-vline.addWidget(label,alignment= Qt.AlignCenter)
-button = QPushButton('нажми')
-button.clicked.connect(show_winer)
-label1 = QLabel('?')
-vline.addWidget(label1,alignment= Qt.AlignCenter)
-vline.addWidget(button,alignment= Qt.AlignCenter)
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connets()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle('Здоровье')
+    def initUI(self):
+        pass
+    def connets(self):
+        pass
 
-mw.setLayout(vline)
-app.exec_()
 
